d98a916f002d80ac855a2122a4e0ca1962ad069d
Lucas <regazzi58@gmail.com>
2021-01-05 12:34:36 -0500

    updated skills section, fixing accessibility flaws :hammer:

--- scripts/index.js
+++ scripts/index.js
@@ -2,11 +2,32 @@ const regazziDev = {};
 
 regazziDev.prependHTMLOnDOM = () => {
     $('body').prepend(`
-    <nav class="scroll"><ul><li class="header"><a href="#Top" id="initialTab" tabindex="0"><span class="long">Back to the top </span><i class="fas fa-arrow-up"></i></a></li> <li class="aboutMe"><a href="#About" tabindex="0">About<span class="long"> Lucas</span></a></li> <li class="skillsIHave"><a href="#Skills" tabindex="0">Skills</a></li> <li class="recentProjects"><a href="#Projects" tabindex="0"><span class="long">Recent </span>Projects</a></li> <li class="contactMe"><a href="#Contact" tabindex="0">Contact</a></li><ul></nav>
-        <header id="Top">
+        <nav class="scroll">
+        <ul>
+            <li class="header"><a href="#Top" id="initialTab" tabindex="0"><span class="long">Back to the top </span><i class="fas fa-arrow-up"></i></a></li>
+            <li class="aboutMe"><a href="#About" tabindex="0">About<span class="long"> Lucas</span></a></li>
+            <li class="skillsIHave"><a href="#Skills" tabindex="0">Skills</a></li>
+            <li class="recentProjects"><a href="#Projects" tabindex="0"><span class="long">Recent </span>Projects</a></li>
+            <li class="contactMe"><a href="#Contact" tabindex="0">Contact</a></li>
+            <ul>
+    </nav>
+    <header id="Top">
         <div class="wrapper">
             <h1>
-                <div class="Lucas"><a href="#About" title="Clicking this navigates to the about section of this webpage">Lucas Regazzi</a></div> <div class="isAnArtistCoder">is a <a href="https://en.wikipedia.org/wiki/Web_developer" target="_blank" title="Clicking this navigates to the wikipedia page for Web Developer">web developer</a></div> <div class="basedInBrooklyn">currently based in <a href="https://en.wikipedia.org/wiki/Brooklyn" target="_blank" title="Clicking this navigates to the wikipedia page for Brooklyn, New York">Brooklyn, USA</a>.</div> <div class="hePrograms">His programmatic</div> <div class="websites"><a href="#Skills">work</a></div> <div class="forArtsSake"><span class="emphatic">expands</span> the <span class="emphatic">accessibility of art</span></div> <div class="throughScreens">through <span class="emphatic">digital means</span>.</div> <div class="ifYoureCurious">Navigate to</div> <div class="viewHisProjects"><a href="#Projects">recent projects</a></div> <div class="toSenseHisVibe">to experience his mission in action,</div> <div class="ya">otherwise</div> <div class="please">feel free to</div> <div class="emailHim"><a href="#Contact" title="Clicking this navigates to the contact form on this webpage">drop him a line</a>.</div>
+                <div class="Lucas"><a href="#About" title="Clicking this navigates to the about section of this webpage.">Lucas Regazzi</a></div>
+                <div class="isAnArtistCoder">is a web developer</div>
+                <div class="basedInBrooklyn">currently based in New York, USA.</div>
+                <div class="hePrograms">His programmatic</div>
+                <div class="websites"><a href="#Skills" title="Clicking this navigates to the skills section of this webpage.">work</a></div>
+                <div class="forArtsSake"><span class="emphatic">expands</span> the <span class="emphatic">accessibility of art</span></div>
+                <div class="throughScreens">through <span class="emphatic">digital means</span>.</div>
+                <div class="ifYoureCurious">Navigate to</div>
+                <div class="viewHisProjects"><a href="#Projects" title="Clicking this navigates to the projects section of this webpage.">recent projects</a></div>
+                <div class="toSenseHisVibe">to experience his mission in action,</div>
+                <div class="ya">otherwise</div>
+                <div class="please">feel free to</div>
+                <div class="peruseCV"><span class="restyle">peruse</span> his <a href="" target="_blank" title="Clicking this navigates to an externally hosted C.V. .pdf document">C.V.</a> or</div>
+                <div class="emailHim"><a href="#Contact" title="Clicking this navigates to the contact form on this webpage.">drop him a line</a>.</div>
             </h1>
         </div>
     </header>
@@ -16,26 +37,32 @@ regazziDev.prependHTMLOnDOM = () => {
                 <img src="./assets/sunnyWaveFinal.gif" alt="An illustration of me waving from my home office." title="An actual picture of me waving to you from my home office.">
             </section>
             <section class="aboutContent">
-                <p>Hey! Lucas here. Prior to becoming a developer I worked in the art world, in both commercial and non-profit sectors. When I'm not designing and creating accessible, beautiful web-based interfaces, I'm in my studio, making paintings, drawings and poems. You can view that portfolio of work <a href="http://lucasregazzi.work" target="_blank" title="A link to my art portfolio.">here</a>.</p>
+                <p>Hey! Lucas here. Prior to becoming a developer I worked in the art world, in both commercial and non-profit sectors. When I'm not designing and creating accessible, beautiful web-based interfaces, I'm in my studio, making paintings, drawings and poems. You can view that portfolio of work <a href="http://lucasregazzi.work" target="_blank" title="Clicking this navigates to my distinctive art portfolio.">here</a>.</p>
             </section>
         </div>
         <div class="wrapper skills" id="Skills">
             <div class="skills">
                 <div class="list">
+                    <i class="fi fi-universal-acces" title="This icon represents accessibility standards.">
+                        <div class="type">Accessibility</div>
+                    </i>
+                    <i class="fi fi-mobile-alt" title="This icon represents mobile/desktop web design responsiveness.">
+                        <div class="type">Responsive Design</div>
+                    </i>
                     <i class="fi fi-html5" title="This is the logo for HTML 5.">
                         <div class="type">HTML 5</div>
                     </i>
                     <i class="fi fi-css3" title="This is the logo for CSS 3.">
                         <div class="type">CSS 3</div>
                     </i>
-                    <i class="fi fi-java" title="This is the logo for Javascript.">
+                    <i class="devicon-javascript-plain" title="This is the logo for JavaScript.">
                         <div class="type">Javascript</div>
                     </i>
                     <i class="fi fi-jquery" title="This is the logo for jQuery.">
                         <div class="type">jQuery</div>
                     </i>
                     <i class="fi fi-sass" title="This is the logo for SCSS/SASS.">
-                            <div class="type">Sass</div>
+                        <div class="type">Sass</div>
                     </i>
                     <i class="fi fi-react" title="This is the logo for React.">
                         <div class="type">React</div>
@@ -52,10 +79,15 @@ regazziDev.prependHTMLOnDOM = () => {
         <div class="wrapper projects" id="Projects">
             <div class="project1">
                 <section class="projectHeading">
-                    <div class="desktopWindow"><a href="https://lucasregazzi.github.io/lucasRegazziProject3/" target="_blank" title="Click here to navigate to the live site for this application."><img src="assets/project3Gif.gif" alt="This is a gif that illustrates my still-life painting application."></a></div>
+                    <div class="desktopWindow"><a href="https://lucasregazzi.github.io/lucasRegazziProject3/"
+                            target="_blank" title="Click here to navigate to the live site for this application."><img
+                                src="assets/project3Gif.gif"
+                                alt="This is a gif that illustrates my still-life painting application."></a></div>
                 </section>
                 <section class="projectContent">
-                    <div class="contentTitle"><h2>Still-life painting generator</h2></div>
+                    <div class="contentTitle">
+                        <h2>Still-life painting generator</h2>
+                    </div>
                     <p>I built this application using jQuery.</p>
                     <p>Its interface facilitates the selection of two objects from two seperate lists to compose a user-generated still-life painting.</p>
                 </section>
@@ -69,12 +101,20 @@ regazziDev.prependHTMLOnDOM = () => {
                     <p>It saves and organizes curatorial projects submitted by the user.</p>
                 </section>
                 <section class="projectHeading">
-                    <div class="desktopWindow"><a href="https://lucasregazzi.github.io/project5/" target="_blank" title="Click here to navigate to the live site for this application."><img src="assets/project5.gif" alt="This is a gif that illustrates my application which functions as an organizational tool for curators."></a></div>
+                    <div class="desktopWindow"><a href="https://lucasregazzi.github.io/project5/" target="_blank"
+                            title="Click here to navigate to the live site for this application."><img
+                                src="assets/project5.gif"
+                                alt="This is a gif that illustrates my application which functions as an organizational tool for curators."></a>
+                    </div>
                 </section>
             </div>
             <div class="project3">
                 <section class="projectHeading">
-                    <div class="desktopWindow"><a href="https://lucasclarkecollab.github.io/lucasClarkeProject4/" target="_blank" title="Click here to navigate to the live site for this application."><img src="assets/project6Clarke.gif" alt="This is a gif that illustrates a weather application I built with another programmer."></a></div>
+                    <div class="desktopWindow"><a href="https://lucasclarkecollab.github.io/lucasClarkeProject4/"
+                            target="_blank" title="Click here to navigate to the live site for this application."><img
+                                src="assets/project6Clarke.gif"
+                                alt="This is a gif that illustrates a weather application I built with another programmer."></a>
+                    </div>
                 </section>
                 <section class="projectContent">
                     <div class="contentTitle">
@@ -91,11 +131,12 @@ regazziDev.prependHTMLOnDOM = () => {
             <form action="https://formspree.io/f/mgepngey" method="POST" id="contact">
                 <label for="contact">Drop me a line</label>
                 <label for="email" class="srOnly">Write your email below</label>
-                <input type="email" id="email" placeholder="What's your email address?" name="_replyto"/>
+                <input type="email" id="email" placeholder="What's your email address?" name="_replyto" />
                 <label for="phone" class="srOnly">Write your phone number below</label>
-                <input type="text" placeholder="Do you have a phone number I can reach you at?" name="phone" id="phone"/>
+                <input type="text" placeholder="Do you have a phone number I can reach you at?" name="phone" id="phone" />
                 <label for="message" class="srOnly">Introduce yourself! I love meeting new people.</label>
-                <textarea name="message" placeholder="Introduce yourself! I love meeting new people." id="message"></textarea>
+                <textarea name="message" placeholder="Introduce yourself! I love meeting new people."
+                    id="message"></textarea>
                 <button type="submit">Send</button>
             </form>
         </div>
