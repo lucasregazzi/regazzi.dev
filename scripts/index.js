8b868cf4c7387d0d5d8ba5390b109cc59b66b37e
Lucas <regazzi58@gmail.com>
2021-01-29 16:57:47 -0500

    link accessibility

--- scripts/index.js
+++ scripts/index.js
@@ -204,6 +204,27 @@ regazziDev.stickyMenu = () => {
     })
 }
 
+regazziDev.linkInNewTabAccessibility = () => {
+    function addNoOpener(link) {
+        let linkTypes = (link.getAttribute('rel') || '').split(' ');
+        if (!linkTypes.includes('noopener')) {
+            linkTypes.push('noopener');
+        }
+        link.setAttribute('rel', linkTypes.join(' ').trim());
+    }
+
+    function addNewTabMessage(link) {
+        if (!link.querySelector('.screen-reader-only')) {
+            link.insertAdjacentHTML('beforeend', '<span class="screen-reader-only">(opens in a new tab)</span>');
+        }
+    }
+
+    document.querySelectorAll('a[target="_blank"]').forEach(link => {
+        addNoOpener(link);
+        addNewTabMessage(link);
+    });
+}
+
 regazziDev.backgroundChangeOnHover = () => {
     $('body').mousemove(function (e) {
         let x = e.clientX;
@@ -232,6 +253,7 @@ regazziDev.backgroundChangeOnHover = () => {
 regazziDev.init = () => {
     regazziDev.prependHTMLOnDOM();
     regazziDev.stickyMenu();
+    regazziDev.linkInNewTabAccessibility();
     regazziDev.backgroundChangeOnHover();
 }
 
